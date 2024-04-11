6ee6f3ed203646996025105610aec89f2991c34d
Marcus Betti <marcusbetti@gmail.com>
2024-04-10 21:17:49 -0300

    Atualizacao de AtivoIntangivelControle

--- CtrlA/src/main/java/com/javali/CtrlA/endpoint/AtivoIntangivelControle.java
+++ CtrlA/src/main/java/com/javali/CtrlA/endpoint/AtivoIntangivelControle.java
@@ -4,11 +4,13 @@ import com.javali.CtrlA.entidades.Ativo;
 import com.javali.CtrlA.entidades.AtivoIntangivel;
 import com.javali.CtrlA.repositorios.AtivoRepositorio;
 import com.javali.CtrlA.repositorios.AtivointangivelRepositorio;
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,11 +51,30 @@ public class AtivoIntangivelControle {
     }
 
     @PutMapping("/atualizacao/{id}")
-    public ResponseEntity<AtivoIntangivel> atualizarAtivoIntangivel(@PathVariable long id, @RequestBody AtivoIntangivel ativoIntangivelAtualizado) {
+    public ResponseEntity<?> atualizarAtivoIntangivel(@PathVariable long id, @RequestBody AtivoIntangivel ativoIntangivelAtualizado) {
         return repositorio.findById(id)
                 .map(ativoIntangivel -> {
-                    repositorio.save(ativoIntangivel);
-                    return new ResponseEntity<>(ativoIntangivel, HttpStatus.OK);
+                    Ativo ativo = ativoRepositorio.findById(ativoIntangivelAtualizado.getAtivo().getId())
+                            .orElseThrow(() -> new RuntimeException("Ativo not found with id " + ativoIntangivelAtualizado.getAtivo().getId()));
+                    ativoIntangivelAtualizado.setAtivo(ativo);
+
+                    BeanUtilsBean notNull=new BeanUtilsBean(){
+                        @Override
+                        public void copyProperty(Object dest, String name, Object value)
+                                throws IllegalAccessException, InvocationTargetException {
+                            if(value!=null){
+                                super.copyProperty(dest, name, value);
+                            }
+                        }
+                    };
+                    try {
+                        notNull.copyProperties(ativoIntangivel, ativoIntangivelAtualizado);
+                    } catch (Exception e) {
+                        System.out.println("Exception while updating intangible asset: " + e.getMessage());
+                        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                    AtivoIntangivel updatedAtivoIntangivel = repositorio.save(ativoIntangivel);
+                    return new ResponseEntity<>(updatedAtivoIntangivel, HttpStatus.OK);
                 }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
