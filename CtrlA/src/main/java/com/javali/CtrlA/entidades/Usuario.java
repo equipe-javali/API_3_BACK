3b30d263980f1acf5c6932865c517ba694a18fe9
Maria Gabriela Mello de Oliveira <magabi1610@gmail.com>
2024-04-12 09:57:58 -0300

    Alteração para listagem dos responsáveis pelo ativo na página dos ativos e remoção da listagem de ativos na página dos usuários.

--- CtrlA/src/main/java/com/javali/CtrlA/entidades/Usuario.java
+++ CtrlA/src/main/java/com/javali/CtrlA/entidades/Usuario.java
@@ -1,9 +1,6 @@
 package com.javali.CtrlA.entidades;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.*;
 import com.javali.CtrlA.entidades.Ativo;
 import com.javali.CtrlA.entidades.UsuarioLogin;
 import jakarta.persistence.*;
@@ -50,7 +47,8 @@ public class Usuario extends RepresentationModel<Usuario> {
     @Column(name = "email", length = 100)
     private String email;
 
-    @JsonManagedReference
+//    @JsonManagedReference
+    @JsonBackReference
     @OneToMany(mappedBy = "idResponsavel")
     private Set<Ativo> ativos = new LinkedHashSet<>();
 
