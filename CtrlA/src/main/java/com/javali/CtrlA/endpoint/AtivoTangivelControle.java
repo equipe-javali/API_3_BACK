7dbe333db893de29a131bafb3dc8b39e4206c1d1
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-05-20 09:26:28 -0300

    11 - Registro do histórico tangível

--- CtrlA/src/main/java/com/javali/CtrlA/endpoint/AtivoTangivelControle.java
+++ CtrlA/src/main/java/com/javali/CtrlA/endpoint/AtivoTangivelControle.java
@@ -3,11 +3,12 @@ package com.javali.CtrlA.endpoint;
 
 import com.javali.CtrlA.entidades.Ativo;
 import com.javali.CtrlA.entidades.AtivoTangivel;
+import com.javali.CtrlA.entidades.HistoricoAtivoTangivel;
 import com.javali.CtrlA.entidades.NotaFiscal;
 import com.javali.CtrlA.entidades.Usuario;
 import com.javali.CtrlA.repositorios.AtivoRepositorio;
 import com.javali.CtrlA.repositorios.AtivotangivelRepositorio;
-
+import com.javali.CtrlA.repositorios.HistoricoAtivoTangivelRepositorio;
 import com.javali.CtrlA.repositorios.NotaFiscalRepositorio;
 import com.javali.CtrlA.repositorios.UsuarioRepositorio;
 import lombok.val;
@@ -21,6 +22,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +30,8 @@ import java.util.Optional;
 @RequestMapping("/ativoTangivel")
 @PreAuthorize("hasAnyAuthority('ADM')")
 public class AtivoTangivelControle {
+    @Autowired
+    private HistoricoAtivoTangivelRepositorio historicoRepositorio;
 
     @Autowired
     private AtivotangivelRepositorio repositorio;
@@ -68,6 +72,48 @@ public class AtivoTangivelControle {
                 .map(ativoTangivel -> {
                     Ativo ativo = ativoRepositorio.findById(ativoTangivel.getAtivo().getId())
                             .orElseThrow(() -> new RuntimeException("Ativo not found with id " + ativoTangivel.getAtivo().getId()));
+                    LocalDate dataAtualizacao = LocalDate.now();
+                    ativo.setUltimaAtualizacao(dataAtualizacao);
+                    
+                    HistoricoAtivoTangivel historicoTangivel = new HistoricoAtivoTangivel();
+                    historicoTangivel.setIdAtivoTangivel(ativoTangivel.getId());
+                    historicoTangivel.setDataAlteracao(dataAtualizacao);
+                    historicoTangivel.setIdAtivo(ativo.getId());
+                    historicoTangivel.setNomeAtivo(ativo.getNome());
+                    historicoTangivel.setMarcaAtivo(ativo.getMarca());
+                    historicoTangivel.setCustoAquisicaoAtivo(ativo.getCustoAquisicao());
+                    historicoTangivel.setDataAquisicaoAtivo(ativo.getDataAquisicao());
+                    historicoTangivel.setGarantiaAtivoTangivel(ativoTangivel.getGarantia());
+                    historicoTangivel.setNumeroIdentificacaoAtivo(ativo.getNumeroIdentificacao());
+                    historicoTangivel.setTipoAtivo(ativo.getTipo());
+                    historicoTangivel.setTagAtivo(ativo.getTag());
+                    historicoTangivel.setGrauImportanciaAtivo(ativo.getGrauImportancia());
+                    historicoTangivel.setStatusAtivo(ativo.getStatus());
+                    historicoTangivel.setDescricaoAtivo(ativo.getDescricao());
+                    historicoTangivel.setTaxaDepreciacaoAtivoTangivel(ativoTangivel.getTaxaDepreciacao());
+                    historicoTangivel.setPeriodoDepreciacaoAtivoTangivel(ativoTangivel.getPeriodoDepreciacao());
+                    historicoTangivel.setUltimaAtualizacaoAtivo(ativo.getUltimaAtualizacao());
+                    historicoTangivel.setCamposPersonalizadosAtivo(ativo.getCamposPersonalizados());
+                    
+                    Usuario responsavel = ativo.getIdResponsavel();
+                    if (responsavel != null) {
+                    	historicoTangivel.setIdUsuario(responsavel.getId());
+                        historicoTangivel.setNomeUsuario(responsavel.getNome());
+                        historicoTangivel.setCpfUsuario(responsavel.getCpf());
+                        historicoTangivel.setNascimentoUsuario(responsavel.getNascimento());
+                        historicoTangivel.setDepartamentoUsuario(responsavel.getDepartamento());
+                        historicoTangivel.setTelefoneUsuario(responsavel.getTelefone());
+                        historicoTangivel.setEmailUsuario(responsavel.getEmail());
+                        historicoTangivel.setStatusUsuario(responsavel.getStatus());	
+                    }
+                    
+                    NotaFiscal notaFiscalHistorico = ativo.getIdNotaFiscal();
+                    if (notaFiscalHistorico != null) {
+                    	historicoTangivel.setIdNotaFiscal(notaFiscalHistorico.getId());
+                    	historicoTangivel.setTipoDocumentoNotaFiscal(notaFiscalHistorico.getTipoDocumento());
+                    	historicoTangivel.setDocumentoNotaFiscal(notaFiscalHistorico.getDocumento());
+                    }
+                    historicoRepositorio.save(historicoTangivel);
 
                     // Fetch the Usuario and NotaFiscal entities from the database if idResponsavel and idNotaFiscal are not null
                     if (ativoTangivelAtualizado.getAtivo().getIdResponsavel() != null) {
